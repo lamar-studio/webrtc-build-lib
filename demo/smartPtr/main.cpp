5a5435f029b2c4f67f46a955d369032721d168b7
LaMar <linzr_mai@163.com>
2021-12-25 15:48:19 +0800

    add smartPtr demo

---
+++ demo/smartPtr/main.cpp
@@ -0,0 +1,60 @@
+﻿
+#include <iostream>
+#include "scoped_refptr.h"
+#include "ref_count.h"
+#include "ref_counted_object.h"
+
+using namespace std;
+using namespace rtc;
+
+class A : public RefCountInterface       /*需要继承自类RefCountInterface*/
+{
+public:
+	A(int i)
+		:data_(i)
+	{
+		cout << __FUNCTION__ << " : " << this << endl;
+	}
+
+	void display()
+	{
+		cout << "data = " << data_ << endl;
+	}
+
+	void set(int i)
+	{
+		data_ = i;
+	}
+
+	~A()
+	{
+		cout << __FUNCTION__ << endl;
+	}
+
+private:
+	int data_;
+};
+
+void func(scoped_refptr<A> sp)
+{
+	sp->display();
+
+	sp->set(200);
+}
+
+int main()
+{
+	/*只见new，不见delete。*/
+	scoped_refptr<A> sp = new RefCountedObject<A>(100);
+
+	/*获取托管对象的地址*/
+	cout << "sp = " << sp.get() << endl;
+
+	sp->display();
+
+	func(sp);
+
+	sp->display();
+
+	return 0;
+}
